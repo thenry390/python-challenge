7a9a46388fbc2c2f91e3222f1757ec4277dcc13e
Thomas Henry <tomjohnhenry@gmail.com>
2020-09-28 19:11:42 -0500

    third push of changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,11 +11,16 @@ import csv
 
 totalMonths=0  # total rows in file minus header
 totalAmount=0  # accumulated total of all profit/loss entries
-#average will be dynamic
 greatestIncreaseAmount=0.0
 greatestDecreaseAmount=0.0
+previousMonth=0.00
+change1=0.00
+change2=0.00
+monthOverMonthChange=[]
 greatestIncreaseList=[]
 greatestDecreaseList=[]
+x=0
+y=0
 
 budget_csv = os.path.join("Resources", "budget_data.csv")
 
@@ -30,25 +35,32 @@ with open(budget_csv) as csv_file:
     # Read through each row of data after the header
     for row in csv_reader:
 
-        # Convert row to float and compare to grams of fiber
-        #if float(row[7]) >= 5:
-        #    print(row)
+#        if (previousMonth > 0):
+        monthOverMonthChange.append(float(row[1])-previousMonth)
+
+        previousMonth = float(row[1])        
+
         totalMonths = totalMonths + 1
-        totalAmount = totalAmount + float(row[1])
+        totalAmount = totalAmount + int(row[1])
         
         if (int(row[1]) > 0):
             if (int(row[1]) > greatestIncreaseAmount):
-                greatestIncreaseAmount=float(row[1])
+                greatestIncreaseAmount=int(row[1])
                 greatestIncreaseList = row
         else:
             if (int(row[1]) < greatestDecreaseAmount):
-                greatestDecreaseAmount=float(row[1])  
+                greatestDecreaseAmount=int(row[1])  
                 greatestDecreaseList = row
             
 print("Financial Analysis")
 print("--------------------------------------")
 print("Total Months: " + str(totalMonths))
 print("Total: $" + str(totalAmount))
-print("Average Change: $" + str(totalAmount/totalMonths))
+
+x=sum(monthOverMonthChange)-monthOverMonthChange[0]
+y = len(monthOverMonthChange)-1
+
+
+print("Average Change: $" + str(round((x/y),2)))
 print("Greatest Increase in Profits: " + str(greatestIncreaseList[0] + " ($" + greatestIncreaseList[1] + ")"))
 print("Greatest Decrease in Profits: " + str(greatestDecreaseList[0] + " ($" + greatestDecreaseList[1] + ")"))
