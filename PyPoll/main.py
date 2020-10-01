a932f48e99d0a583562311e69083e250090fcf3b
Thomas Henry <tomjohnhenry@gmail.com>
2020-10-01 11:30:07 -0500

    vote tabulation added

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,17 +4,17 @@ Created on Sun Sep 27 08:46:24 2020
 
 @author: thenry
 """
-#added a test to make sure push works in git
-
 import os, csv
 
 totalVotesCast=0  # total rows in file minus header
-
-x=0
-y=0
+voteTabulation=[]
+indexOfRow=0
+voteCount=0
 reportList=[]
+maxVoteCount=0
+winner=""
 
-election_csv = os.path.join("Resources", "election_data.csv")
+election_csv = os.path.join("Resources", "election_data_sample.csv")
 # Specify the file to write to
 output_path = os.path.join("analysis", "analysis_output.csv")
 
@@ -29,11 +29,28 @@ with open(election_csv) as csv_file:
     for row in csv_reader:
 
         totalVotesCast = totalVotesCast + 1
-
+        try:
+            indexOfRow=voteTabulation.index(row[2])
+        except ValueError:
+            indexOfRow=-1
+            
+        if (indexOfRow==-1):
+            voteTabulation.append(row[0])
+            voteTabulation.append(row[2])
+            voteTabulation.append(1)
+        else:
+            voteCount=int(voteTabulation[indexOfRow+1])+1
+            voteTabulation[indexOfRow+1]=voteCount
+            
+            if(voteCount>maxVoteCount):
+                maxVoteCount=voteCount
+                winner=voteTabulation[indexOfRow]
+            
 reportList.append("Election Results")
 reportList.append("--------------------------------------")
 reportList.append("Total Votes: " + str(totalVotesCast))
 reportList.append("--------------------------------------")
+
 reportList.append("--------------------------------------")
 reportList.append("--------------------------------------")
 
