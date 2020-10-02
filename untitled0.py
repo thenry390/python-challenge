817b27673634c4b22504b6ace4322b7594de067a
Thomas Henry <tomjohnhenry@gmail.com>
2020-10-01 22:15:39 -0500

    restore README.md

---
+++ untitled0.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 26 16:47:38 2020
+
+@author: thenry
+"""
+import csv
+import os
+import timeit
+import pandas as pd
+
+#filename = open('C:\Users\thenry\bootcamp\homework\python-challenge\PyBank\Resources\budget_data.csv')
+#open(filename, 'r') as csvfile:
+
+    # Split the data on commas
+    #csvreader = csv.reader(csvfile, delimiter=',')
+#filename = open('C:\Users\thenry\bootcamp\homework\python-challenge\PyBank\Resources\budget_data.csv', 'r',newline='') as csvfile
+f = open('C:\Users\thenry\bootcamp\homework\python-challenge\PyBank\Resources\budget_data.csv', 'r'):
+    sum(1 for line in f)
